6551e56586a2f29b0b9f9f92871cecbded35f9fc
Francisco Javier <fjmarche280900@gmail.com>
2026-04-22 12:16:56 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,18 @@ public class Main {
         System.out.println("MatchPoint iniciado");
     }
 }
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Connection conn = DatabaseConnection.getConnection();
+
+        if (conn != null) {
+            System.out.println("Conexión OK");
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
