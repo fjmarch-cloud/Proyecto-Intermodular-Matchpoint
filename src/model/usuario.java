133f57711169eb8f9aceefbd69691955c02fcd45
Francisco Javier <fjmarche280900@gmail.com>
2026-04-23 11:32:25 +0200

    model/usuario.java

---
+++ src/model/usuario.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getEmail() { return email; }
+}
